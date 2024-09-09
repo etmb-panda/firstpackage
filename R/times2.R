f394b9632659db9dda1fc4ed53f751724dc57c99
etmb-panda <etmboyetey@gmail.com>
2024-09-09 15:12:43 +0000

    Initial commit

---
+++ R/times2.R
@@ -0,0 +1,11 @@
+#' Title function to double a number
+#'
+#' @param x a numeric value
+#'
+#' @return The nuber multiplied by 2
+#' @export
+#'
+#' @examples times2(5)
+times2 <- function(x){
+  x*2
+}
