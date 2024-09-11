457a60117419f74e3776f392c500040c614c1ebb
DoraBoadi <dora.boadi@yahoo.com>
2024-09-11 13:02:33 +0000

    updated the function

--- R/times2.R
+++ R/times2.R
@@ -6,6 +6,25 @@
 #' @export
 #'
 #' @examples times2(5)
-times2 <- function(x){
-  x*2
+times2 <- function(x) {
+  if (!is.numeric(x)) {
+    stop("Error: Input must be numeric.")
+  }
+  x * 2
+}
+
+
+#' Title
+#'
+#' @param x a numeric value
+#'
+#' @return The nuber multiplied by 3
+#' @export
+#'
+#' @examples times3(5)
+times3 <- function(x) {
+  if (!is.numeric(x)) {
+    stop("Error: Input must be numeric.")
+  }
+  x * 3
 }
---
+++ tests/testthat/test-times2.R
@@ -0,0 +1,5 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+  expect_equal(2 * 3, 6)
+  expect_no_error(2)
+})
